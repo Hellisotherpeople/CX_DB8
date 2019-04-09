c98107c976149d818818b0bc6362c7e254cbac10
Allen Roush <gedboy2112@gmail.com>
2019-04-09 12:05:48 -0700

    Add files via upload

--- cx_db8_flair.py
+++ cx_db8_flair.py
@@ -5,44 +5,57 @@ from flair.embeddings import WordEmbeddings, FlairEmbeddings, StackedEmbeddings,
 import torch
 # create a StackedEmbedding object that combines glove and forward/backward flair embeddings
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
+from sklearn.metrics import jaccard_similarity_score
+#import numpy as np
 from docx import Document 
+import sys
+import numpy as np
 
-document = Document()
 
 
-class underline:
+
+
+document = Document() ## Create a python-docx document 
+
+
+class underline: ## weird stuff to make python show underlining and highlighting in it's printed output - kinda neat
     start_underline='\033[04m'
     end_underline = '\033[0m'
     start_highlight = '\x1b[4;37;45m'
 
 
+"""
+###### The Stacked Embeddings are the list of word embeddings which we choose for converting a word window into a "meaning" vector. #####
+
+To enable a set of embeddings, remove the pound sign "#" from the beginning of the Embedding. 
+
+FAST: To summarize extremely fast, enable on the "WordEmbeddings" selections (en is fasttext, extvec is a skip-gram model) 
+MEDIUM: To summarize with much more semantic understanding but running still relatively quickly, enable the "FlairEmbeddings" with the word "fast" at the end (this is the default). This is a lot slower than "FAST" but has dramatically improved abilities to understand meaning. 
+SLOW: To summarize with the state-of-the-art, but much more time consuming concatonation of BERT, Flair, and FastText, uncomment the "BertEmbeddings" layer, and remove the word "-fast" from the end of "news-forward-fast" and "news-backward-fast". This is about 2x as slow  as the medium settings 
+
+
+"""
 stacked_embeddings = DocumentPoolEmbeddings([
-                                        WordEmbeddings('en'),#WordEmbeddings('glove'),
+                                        WordEmbeddings('en'),
+                                        #WordEmbeddings('glove'),
                                         WordEmbeddings('extvec'),#ELMoEmbeddings('original'),
                                         #BertEmbeddings('bert-base-cased'),
-                                        #FlairEmbeddings('news-forward-fast'),
-                                        #FlairEmbeddings('news-backward-fast')
-                                       ])
+                                        FlairEmbeddings('news-forward-fast'),
+                                        FlairEmbeddings('news-backward-fast'), 
+                                       ])#, mode='max')
 
 def set_card():
-    card = input("Input the Card Text: ")
+    print("Input the Card Text, press Ctrl-D to end text entry")
+    card = sys.stdin.read()#input("Input the Card Text: ")
     card_tag = input("Input the card_tag, or a -1 to summarize in-terms of the card itself: ")
     card = str(card)
-    tag_str = str(card_tag)
-    if str(card_tag) == "-1":
-        print("YOU DIDN'T USE A CARD")
+    if str(card_tag) == "-1": #This will not work with large documents when bert is enabled
         card_tag = Sentence(str(card))
-    else:
+        tag_str = ""
+    else:    
+        tag_str = str(card_tag)
         card_tag = Sentence(str(card_tag))
     return card, card_tag, tag_str
-
-#card_tag = Sentence('The Executive Order harms health, safety, and the environment')
-
-"""
-card = 
-The Executive Order will block or force the repeal of regulations needed to protect health, safety, and the environment, across a broad range of topics—from automobile safety, to occupational health, to air pollution, to endangered species. 4. The Executive Order mandates that, when implementin g the command to repeal at least two rules for each new one, agencies must focus on costs while ignoring benefits. Indeed, the Executive Order directs agencies to disregard the benefits of new and existing rules— including benefits to consumers, to workers, to people exposed to pollution, and to the economy—even when the benefits far exceed costs. The Executive Order’s direction to federal agencies to zero out costs to regulated industries, while entirely ignoring benefits to the Americans whom Congress enacted these statutes to protect, will force agencies to take regulatory actions that harm the people of this nation. 5. To repeal two regulations for the purpose of adopting one new one, based solely on a directive to impose zero net costs and without any consideration of benefits, is arbitrary, capricious, an abuse of discretion, and not in accordance with law, for at least three reasons. First, no governing statute authorizes any agency to withhold a regulation intended to address identified harms to public safety, health, or other statutory objectives on the basis of an arbitrary upper limit on total costs (for fiscal year 2017, a limit of $0) that regulations may impose on regulated entities or the economy. Second, the Executive Order forces agencies to repeal regulations that they have already determined, through notice-and-comment rulemaking, advance the purposes of the underlying statutes, and forces the agencies to do so for the sole purpose of eliminating costs that the underlying statutes do not direct be eliminated. Third, no governing statute authorizes an agency to base its actions on a decisionmaking criterion of zero net cost across multiple regulations. Case 1:17-cv-00253 Document 1 Filed 02/08/17 Page 4 of 49 5 6. Rulemaking in compliance with the Executive Order’ s “1-in, 2-out” requirement cannot be undertaken without violating the statutes from which the agencies derive their rulemaking authority and the Administrative Procedure Act (APA). 7. The implementation of governing statutes, passed by Congress and signed into law by previous Presidents, will slow to a halt under the Executive Order. In addition to complying with the substantive requirements of thos e laws and the procedural requirements of the APA, agencies, to issue a new proposed or final rule, will be required to undertake new cost assessments both of the new proposed or final rule and at least two existing rules—although the new rule and the existing rules need not have any substantive relationship to one another and, with approval from OMB, need not even be issued by the same agency.
-"""
 
 
 def create_ngram(num_context, card, card_tag):
@@ -66,13 +79,6 @@ def create_ngram(num_context, card, card_tag):
     card_words = ngram_list
     return card_words, card_words_org
 
-#print(card_words)
-'''
-def find_ngrams(input_list, n):
-    return zip(*[input_list[i:] for i in range(n)])
-'''
-# just embed a sentence using the StackedEmbedding as you would with any single embedding.
-
 
 
 
@@ -95,6 +101,7 @@ def embed(card_tag, card_as_sentence, card_words, card_words_org):
         '''
         stacked_embeddings.embed(n_gram_word)
         word_sim = cosine_similarity(card_tag_emb, n_gram_word.get_embedding().reshape(1,-1))
+        #word_sim = jaccard_similarity_score(card_tag_emb, n_gram_word.get_embedding().reshape(1,-1), normalize=True)
         word_tup = (card_words_org[count], word_sim)
         count += 1
         word_list.append(word_tup)
@@ -140,15 +147,14 @@ def parse_word_val_list(word_list, h, n):
             sum_str += " "
             removed_str += str(sum_word[0])
             removed_str += " "
-    document.save('test_sum.docx')
     return sum_str, removed_str, token_removed_ct
 
 def new_sum(word_list):
     val_list = summarize(word_list)
     th_9 = np.percentile(val_list, 90)
-    th_75 = np.percentile(val_list, 75)
+    th_75 = np.percentile(val_list, 60)
     th_50 = np.percentile(val_list, 50)
-    th_25 = np.percentile(val_list, 25)
+    th_25 = np.percentile(val_list, 35)
     th_10 = np.percentile(val_list, 10)
     sum_str, removed_str, token_removed_ct = parse_word_val_list(word_list, th_75, th_25)
 
@@ -164,11 +170,22 @@ def new_sum(word_list):
 
     print(removed_str)
 
-card, card_tag, tag_str = set_card()
 
-head = document.add_heading(tag_str, level=1)
+for i in range(0, 1000):
+    cont = input("Do you want to continue? type y for yes else for stop!")
+    if cont == "y":
+        card, card_tag, tag_str = set_card()
+        head = document.add_heading(tag_str, level=1)
+        card_auth = input("what's the card author and date?")
+        card_cite = input("what's the citation?")
+        a_par = document.add_paragraph()
+        a_par.add_run(card_auth).bold = True
+        c_par = document.add_paragraph(card_cite)
+        par = document.add_paragraph()
+        word_list = run_loop(10, card, card_tag)
+        new_sum(word_list)
+    else:
+        document.save('test_sum.docx')
+        break
 
-par = document.add_paragraph()
-word_list = run_loop(10, card, card_tag)
-new_sum(word_list)
 
